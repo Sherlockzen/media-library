ebe0e9614be7fb551ad5ea2e06bfaf5b146ad7d1
Hewerton <hewertonlourenco@gmail.com>
2024-01-28 00:34:57 -0300

    refactor(validation): creating schema for validation of the user signin and signup

---
+++ src/schema/index.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+ name: z
+  .string()
+  .min(3, {
+   message: "Campo usuário precisa ter no mínimo 3 caracteres",
+  })
+  .max(30, {
+   message: "Campo usuário pode ter no máximo 30 caracteres",
+  }),
+ password: z
+  .string()
+  .min(8, {
+   message: "Campo de senha precisa ter no mínimo 8 caracteres",
+  })
+  .max(80, {
+   message: "Campo de senha suporta no máximo 80 caracteres",
+  }),
+ email: z.string().email({ message: "Digite um email valido" }),
+});
+
+export const signinSchema = z.object({
+ email: z.string().email({ message: "Digite um email valido" }),
+ password: z
+  .string()
+  .min(8, {
+   message: "Campo de senha precisa ter no mínimo 8 caracteres",
+  })
+  .max(80, {
+   message: "Campo de senha suporta no máximo 80 caracteres",
+  }),
+});

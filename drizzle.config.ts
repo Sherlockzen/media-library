a5d312fb8e126e67e0275abbb0fafac196e90808
Hewerton <hewertonlourenco@gmail.com>
2024-01-27 23:58:27 -0300

    fix(config): fixing the path in drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,11 @@ import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
- schema: "./src/app/db/db.ts",
- out: "./src/app/db/migrations",
+ schema: "./src/server/db/schema.ts",
+ out: "./src/server/db/migrations",
  driver: "pg",
  dbCredentials: {
   connectionString: process.env.DATABASE_URL!,
  },
+ tablesFilter: ["dz_*"],
 } satisfies Config;

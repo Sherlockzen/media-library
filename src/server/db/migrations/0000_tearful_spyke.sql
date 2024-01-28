18c892723d38491548ad56bd0d0047faf6f3a768
Hewerton <hewertonlourenco@gmail.com>
2024-01-28 00:37:07 -0300

    chore(schema): some schemas changes

---
+++ src/server/db/migrations/0000_tearful_spyke.sql
@@ -0,0 +1,38 @@
+DO $$ BEGIN
+ CREATE TYPE "role" AS ENUM('ADMIN', 'USER');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "midia-library_midia" (
+	"id" text PRIMARY KEY NOT NULL,
+	"owner_id" text NOT NULL,
+	"title" text NOT NULL,
+	"url" text NOT NULL,
+	"size" integer NOT NULL,
+	"type" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "midia-library_session" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "midia-library_user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	"role" "role" DEFAULT 'USER',
+	"quota" integer DEFAULT 1000,
+	"used_quota" integer DEFAULT 0
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "midia-library_session" ADD CONSTRAINT "midia-library_session_user_id_midia-library_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "midia-library_user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;

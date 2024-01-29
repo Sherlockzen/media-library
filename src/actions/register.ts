ddaa7b2a0cd8a75960294ec42364081a192d6d36
Hewerton <hewertonlourenco@gmail.com>
2024-01-29 11:20:10 -0300

    fix(route): fixing the route to authorized

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -12,43 +12,43 @@ import { Argon2id } from "oslo/password";
 import { z } from "zod";
 
 export async function signup(values: z.infer<typeof signupSchema>) {
- const validateValues = signupSchema.safeParse(values);
+  const validateValues = signupSchema.safeParse(values);
 
- if (!validateValues.success) {
-  return {
-   error: "Erro ao cadastrar usuario",
-  };
- }
+  if (!validateValues.success) {
+    return {
+      error: "Erro ao cadastrar usuario",
+    };
+  }
 
- const { email, password, name } = validateValues.data;
+  const { email, password, name } = validateValues.data;
 
- const hashedPassword = await new Argon2id().hash(password);
+  const hashedPassword = await new Argon2id().hash(password);
 
- const existUser = await db.query.userTable.findFirst({
-  where: eq(userTable.email, email),
- });
+  const existUser = await db.query.userTable.findFirst({
+    where: eq(userTable.email, email),
+  });
 
- if (existUser) {
-  return {
-   error: "Usuario ja existe",
-  };
- }
+  if (existUser) {
+    return {
+      error: "Usuario ja existe",
+    };
+  }
 
- const userId = generateId(15);
+  const userId = generateId(15);
 
- await db.insert(userTable).values({
-  id: userId,
-  name: name,
-  password: hashedPassword,
-  email: email,
- });
+  await db.insert(userTable).values({
+    id: userId,
+    name: name,
+    password: hashedPassword,
+    email: email,
+  });
 
- const session = await lucia.createSession(userId, {});
- const sessionCookie = lucia.createSessionCookie(session.id);
- cookies().set(
-  sessionCookie.name,
-  sessionCookie.value,
-  sessionCookie.attributes
- );
- return redirect("/");
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+  return redirect("/authorized");
 }

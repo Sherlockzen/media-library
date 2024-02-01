9009c6c61cddfbd1003d832b9cbc9240f2c6e8f3
Hewerton <hewertonlourenco@gmail.com>
2024-02-01 19:48:37 -0300

    refact(route): changing to router refresh to update the page

--- src/actions/signout.ts
+++ src/actions/signout.ts
@@ -4,20 +4,20 @@ import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
 export async function logout() {
- const { session } = await validateRequest();
- if (!session) {
-  return {
-   error: "Não autorizado",
-  };
- }
+  const { session } = await validateRequest();
+  if (!session) {
+    return {
+      error: "Usuário já está deslogado",
+    };
+  }
 
- await lucia.invalidateSession(session.id);
+  await lucia.invalidateSession(session.id);
 
- const sessionCookie = lucia.createBlankSessionCookie();
- cookies().set(
-  sessionCookie.name,
-  sessionCookie.value,
-  sessionCookie.attributes
- );
- return redirect("/login");
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+  return { success: 'Logout realizado!' };
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,22 @@
+"use client";
 import React from "react";
 import { Button } from "./ui/button";
-import { validateRequest } from "@/server/auth";
 import { logout } from "@/actions/signout";
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
 
-async function Navbar() {
- const { user } = await validateRequest();
+function Navbar() {
+ const [isPending, startTransition] = useTransition();
+ const router = useRouter();
+ function handleLogout() {
+  startTransition(() => {
+   logout().then((result) => {
+    if (!result.error) {
+     router.push("/");
+    }
+   });
+  });
+ }
  return (
   <div className=" w-full h-14 border-b flex gap-4 items-center justify-between px-6">
    <div className=" flex gap-8"></div>
